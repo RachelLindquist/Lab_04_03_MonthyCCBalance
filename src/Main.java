a88a6d1aa0131892dab330cfb46c03c5216bfdcb
RachelLindquist <firefly7491@gmail.com>
2022-06-05 13:52:15 -0400

    Lab 4 part 2 MonthlyCCBalance complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double INTEREST_RATE = .17;
+
+        double initialBalance = 5000.00;
+
+        double month1Balance = initialBalance + initialBalance * INTEREST_RATE;
+        double month2Balance = month1Balance + month1Balance * INTEREST_RATE;
+
+        System.out.println("With an initial balance of " + initialBalance + " dollars, after one month the balance will be " +
+                month1Balance + " dollars, and after two months the balance will be " + month2Balance + " dollars.");
     }
 }
